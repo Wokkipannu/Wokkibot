0530c79ecbdd2e8b841147c05b26d7d80cc321d5
Wokkipannu <iiro@kooditaikuri.fi>
2019-11-01 04:45:54 +0200

    Removed random responses for unknown commands

--- commands/util/unknown.js
+++ commands/util/unknown.js
@@ -13,19 +13,6 @@ module.exports = class UnknownCommandCommand extends Command {
   }
 
   run(msg) {
-    const possibilities = [
-      "mutta nyt :zipper_mouth:",
-      "Vittu säkin olet lapsellinen? Kysyn kiltisti jotain niin tommonen vitu chättibodyguard ei voi ottaa chat banneja pois mitkä oon saanu aivan vitun turhaan kun heitin läppää?",
-      "http://www.tilaapullo.com/",
-      "Hei, olen Tohtori Gerhard",
-      "Jopa 8cm pidempi penis kahdessa viikossa",
-      "mee lahjottaan tonni hyväntekeväisyyteen ja tuu sitten neuvoon elämässä",
-      "new to this channel <----"
-    ];
-
-    const random = Math.floor(Math.random() * possibilities.length);
-    const quote = possibilities[random];
-
-    return msg.reply(quote);
+    return;
   }
 }
