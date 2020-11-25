acbe7a686674d54e613d3d3770ea6e6c72e4f1fd
Wokki <iiro@kooditaikuri.fi>
2020-11-25 17:07:50 +0200

    Simplified and fixed play command

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,6 +1,6 @@
 const { Command } = require('discord.js-commando');
 const { Collection } = require('discord.js');
-const ytdl = require('ytdl-core-discord');
+const ytdl = require("discord-ytdl-core");
 
 module.exports = class PlayCommand extends Command {
   constructor(client) {
@@ -16,6 +16,12 @@ module.exports = class PlayCommand extends Command {
           key: 'url',
           prompt: 'Enter URL or keyword',
           type: 'string'
+        },
+        {
+          key: 'bass',
+          prompt: '',
+          type: 'string',
+          default: '10'
         }
       ]
     });
@@ -23,127 +29,63 @@ module.exports = class PlayCommand extends Command {
     this.queue = new Collection();
   }
 
-  async run(msg, { url }) {
-    const channel = msg.member.voice.channel;
-    if (!channel) return msg.reply('You can only use play command when in a voice channel');
+  async run(msg, { url, bass }) {
+    if (!msg.member.voice.channel) return msg.channel.send("You must be connected to voice channel to play");
 
-    msg.reply('Handling song request').then(async message => {
-      // Get queue
-      const queue = await this.queue.get(msg.guild.id);
+    let queue = await this.queue.get(msg.guild.id);
 
-      // If URL is youtube URL
-      if (url.match(/^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/g)) {
-        await ytdl.getInfo(url)
-          .then(async info => {
-            const song = {
-              type: 'yt',
-              id: info.video_id,
-              url: info.video_url,
-              title: info.title,
-              length: info.length_seconds,
-              requestedBy: msg.author
-            };
+    if (url.match(/^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/g)) {
+      const song = {
+        url,
+        bass
+      };
 
-            if (queue) {
-              queue.songs.push(song);
-            }
-            else {
-              await channel.join()
-                .then(async connection => {
-                  const newQueue = {
-                    connection: connection,
-                    songs: [song]
-                  };
-
-                  await this.queue.set(msg.guild.id, newQueue);
-                  this.play(connection);
-                })
-                .catch(error => {
-                  console.error(error);
-                  message.edit('Failed to join your voice channel');
-                });
-            }
-            message.edit(`${info.title} added to queue`);
-          })
-          .catch(error => {
-            console.error(error);
-            return message.edit('Error occurred while trying to get song info');
-          });
+      if (queue) {
+        await queue.songs.push(song);
+        msg.reply("Added to queue");
       }
       else {
-        if (url.endsWith('.mp3')) {
-          const song = {
-            type: 'mp3',
-            id: 'N/A',
-            url: url,
-            title: 'Unknown',
-            length: 0,
-            requestedBy: msg.author
-          };
-
-          if (queue) {
-            queue.songs.push(song);
-          }
-          else {
-            await channel.join()
-              .then(async connection => {
-                const newQueue = {
-                  connection: connection,
-                  songs: [song]
-                };
-
-                await this.queue.set(msg.guild.id, newQueue);
-                this.play(connection);
-              })
-              .catch(error => {
-                console.error(error);
-                message.edit('Failed to join your voice channel');
-              });
-          }
-
-          message.edit(`mp3 file added to queue`);
-        }
-        else {
-          return message.edit('URL has to be YouTube video or mp3 file');
-        }
+        await this.queue.set(msg.guild.id, { id: msg.guild.id, msg, songs: [song], connection: undefined });
+        queue = await this.queue.get(msg.guild.id);
+        msg.reply("Added to queue");
+        return this.play(queue);
       }
-    });
-  }
-
-  async play(connection) {
-    const queue = await this.queue.get(connection.channel.guild.id);
-    if (queue.songs.length === 0) {
-      connection.channel.leave();
-      this.queue.delete(connection.channel.guild.id);
     }
     else {
-      console.log(`Starting to play song ${queue.songs[0].title}`);
-      if (queue.songs[0].type === 'yt') {
-        connection.play(await ytdl(queue.songs[0].url), { type: 'opus', volume: 0.1 })
-          .on('finish', () => {
-            console.log('Song ended or it was skipped');
-            queue.songs.shift();
-            this.play(connection);
-          })
-          .on('error', (error) => {
-            console.error(error);
-            queue.songs.shift();
-            this.play(connection);
-          });
-      }
-      else {
-        connection.play(queue.songs[0].url)
-          .on('finish', () => {
-            console.log('Song ended or it was skipped');
-            queue.songs.shift();
-            this.play(connection);
-          })
-          .on('error', (error) => {
-            console.error(error);
-            queue.songs.shift();
-            this.play(connection);
-          });
-      }
+      return msg.channel.send("Play command must be supplied with a youtube link");
     }
+  }
+
+  async play(queue) {
+    const { msg } = queue;
+
+    if (queue.songs.length === 0) {
+      msg.channel.send("No more songs in queue");
+      queue.connection.channel.leave();
+      return this.queue.delete(queue.id);
+    }
+
+    const { url, bass } = queue.songs[0];
+
+    let stream = await ytdl(url, {
+      filter: "audioonly",
+      opusEncoded: false,
+      fmt: "mp3",
+      encoderArgs: ['-af', `bass=g=${bass},dynaudnorm=f=200`]
+    });
+
+    if (!queue.connection || queue.connection === undefined) {
+      await msg.member.voice.channel.join().then(connection => queue.connection = connection);
+    }
+
+    let dispatcher = queue.connection.play(stream, {
+      type: "unknown",
+      volume: 0.1
+    })
+    .on("finish", () => {
+      queue.songs.shift();
+      return this.play(queue);
+    });
+
   }
 }
