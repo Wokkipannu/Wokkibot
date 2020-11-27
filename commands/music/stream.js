2de69200f2573215ee3ae765c12db7ad3b4c192d
Wokki <iiro@kooditaikuri.fi>
2020-11-27 16:48:40 +0200

    Experimental stream command

---
+++ commands/music/stream.js
@@ -0,0 +1,39 @@
+module.exports = class StreamCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'stream',
+      group: 'music',
+      memberName: 'stream',
+      description: 'Play a stream',
+      guildOnly: true,
+      clientPermissions: ['CONNECT', 'SPEAK', 'SEND_MESSAGES'],
+      args: [
+        {
+          key: 'url',
+          prompt: 'URL to stream',
+          type: 'string'
+        }
+      ]
+    })
+  }
+
+  async run(msg, { url }) {
+    if (!msg.member.voice.channel) return msg.channel.send("You must be connected to voice channel to play");
+
+    let stream = ytdl.arbitraryStream(url, {
+      opusEncoded: false,
+      fmt: "mp3",
+      encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
+    });
+
+    msg.member.voice.channel.join()
+      .then(connection => {
+        let dispatcher = connection.play(stream, {
+          type: 'unknown'
+        })
+        .on('finish', () => {
+          msg.guild.me.voice.channel.leave();
+        });
+      });
+  }
+}
