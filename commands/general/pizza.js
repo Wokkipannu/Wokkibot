a4283e6e9b4e69ffbd3edfa471fed7ec2e9b0c4c
Wokki <iiro@kooditaikuri.fi>
2021-04-09 23:01:25 +0300

    Random pizza toppings command

---
+++ commands/general/pizza.js
@@ -0,0 +1,37 @@
+const { Command } = require('discord.js-commando')
+const toppings = require('../../toppings.json')
+
+module.exports = class PizzaCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'pizza',
+      group: 'general',
+      memberName: 'pizza',
+      description: 'Get random pizza toppings',
+      guildOnly: false,
+      clientPermissions: ['SEND_MESSAGES'],
+      args: [
+        {
+          key: 'amount',
+          prompt: 'How many toppings?',
+          type: 'integer',
+          default: 4
+        }
+      ]
+    })
+  }
+
+  run (msg, { amount }) {
+    if (amount > 10) amount = 10
+
+    let selectedToppings = []
+
+    for (let i = 0; i < amount; i++) {
+      selectedToppings.push(toppings[i])
+    }
+
+    let lastTopping = selectedToppings.splice(selectedToppings.length - 1, 1)[0]
+
+    return msg.reply(`Pizzaasi tuli täytteet: ${selectedToppings.join(', ')} ja ${lastTopping}`)
+  }
+}
