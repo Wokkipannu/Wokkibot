cd32a89f55e21cfe00301df21f97602222bde53d
Wokki <iiro@kooditaikuri.fi>
2021-11-23 20:47:27 +0200

    New: NumberFormat utility to format a number
    This is mainly used for formatting the duration to add a 0 in front of numbers lower than 10

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,10 @@
 package utils
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 func InteractionRespondMessage(s *discordgo.Session, i *discordgo.InteractionCreate, msg string) error {
 	return s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
@@ -19,4 +23,12 @@ func InteractionRespondMessageEmbed(s *discordgo.Session, i *discordgo.Interacti
 			Embeds:  []*discordgo.MessageEmbed{embed},
 		},
 	})
+}
+
+func NumberFormat(value uint) string {
+	if value < 10 {
+		return fmt.Sprintf("0%v", value)
+	} else {
+		return fmt.Sprintf("%v", value)
+	}
 }
