2dacfa424a37fd0b7aa96210a5bbf599e4d521f8
Wokki <iiro@kooditaikuri.fi>
2022-03-06 22:41:47 +0200

    New: Send an embed for skipping

--- commands/skip.go
+++ commands/skip.go
@@ -15,6 +15,23 @@ var skip = Command{
 	},
 	Run: func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		if q, found := utils.Queue[i.GuildID]; found {
+			if q.Queue[0].Interaction != nil {
+				Session.InteractionResponseEdit(Session.State.User.ID, q.Queue[0].Interaction, &discordgo.WebhookEdit{
+					Content:    "",
+					Embeds:     []*discordgo.MessageEmbed{q.Queue[0].Embed},
+					Components: []discordgo.MessageComponent{},
+				})
+			}
+			if q.Queue[0].Message != nil {
+				content := ""
+				Session.ChannelMessageEditComplex(&discordgo.MessageEdit{
+					ID:         q.Queue[0].Message.ID,
+					Channel:    q.Queue[0].Message.ChannelID,
+					Content:    &content, // Why the fuck is this *string?
+					Embeds:     []*discordgo.MessageEmbed{q.Queue[0].Embed},
+					Components: []discordgo.MessageComponent{},
+				})
+			}
 			Conn.Stop(i.GuildID)
 			if err := utils.InteractionRespondMessage(s, i, fmt.Sprintf("Track \"%v\" skipped", utils.EscapeString(q.Queue[0].TrackInfo.Title))); err != nil {
 				log.Print(err)
