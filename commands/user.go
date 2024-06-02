d679cb1b54b7288ee32fb91457a4e10bcf3722b0
Wokki <iiroseppanen@protonmail.com>
2024-06-03 00:11:11 +0300

    Added more information to the user command. Fixed cases where user embed would not post for bots.

--- commands/user.go
+++ commands/user.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"fmt"
 	"log/slog"
+	"strings"
 	"time"
 	"wokkibot/wokkibot"
 
@@ -34,32 +35,64 @@ func HandleUser(b *wokkibot.Wokkibot) handler.CommandHandler {
 			user = e.User()
 		}
 
-		// For some reason, user does not contain certain attributes, such as BannerURL or AccentColor, so we must fetch the user from the client
+		// For some reason, user does not contain certain attributes, such as BannerURL or AccentColor, so we must fetch the user from the client rest
 		fetchedUser, err := b.Client.Rest().GetUser(user.ID)
 		if err != nil {
 			slog.Info("Error fetching user from client")
 		}
+		if fetchedUser != nil {
+			user = *fetchedUser
+		}
+
+		displayFlags := map[discord.UserFlags]string{
+			discord.UserFlagActiveDeveloper:           "Active developer",
+			discord.UserFlagBugHunterLevel1:           "Bug hunter level 1",
+			discord.UserFlagBugHunterLevel2:           "Bug hunter level 2",
+			discord.UserFlagDiscordCertifiedModerator: "Discord certified moderator",
+			discord.UserFlagDiscordEmployee:           "Discord employee",
+			discord.UserFlagEarlySupporter:            "Early supporter",
+			discord.UserFlagEarlyVerifiedBotDeveloper: "Early verified bot developer",
+			discord.UserFlagHouseBalance:              "House balance",
+			discord.UserFlagHouseBravery:              "House bravery",
+			discord.UserFlagHouseBrilliance:           "House brilliance",
+			discord.UserFlagHypeSquadEvents:           "Hype squad events",
+			discord.UserFlagPartneredServerOwner:      "Partnered server owner",
+			discord.UserFlagTeamUser:                  "Team user",
+			discord.UserFlagVerifiedBot:               "Verified bot",
+		}
+		var userFlags []string
+
+		for flag, name := range displayFlags {
+			if user.PublicFlags&flag != 0 {
+				userFlags = append(userFlags, name)
+			}
+		}
 
 		embed := discord.NewEmbedBuilder()
-		if fetchedUser.Bot {
-			embed.SetTitlef("%v 🤖", fetchedUser.EffectiveName())
+		embed.SetAuthor(fmt.Sprintf("%v's profile", user.EffectiveName()), "", *user.AvatarURL())
+		if user.Bot {
+			embed.AddField("Type", "Bot", true)
 		} else {
-			embed.SetTitle(fetchedUser.EffectiveName())
+			embed.AddField("Type", "User", true)
 		}
-		embed.AddField("Nickname", *fetchedUser.GlobalName, false)
+		embed.AddField("Global name", user.EffectiveName(), true)
+		embed.AddField("Username", user.Username, true)
+		if len(userFlags) > 0 {
+			embed.AddField("Badges", strings.Join(userFlags, ", "), true)
+		}
 		embed.AddField("Joined this server", fmt.Sprintf("%v (%v days ago)", e.Member().JoinedAt.Format("02.01.2006"), DaysSince(e.Member().JoinedAt)), false)
-		embed.SetThumbnail(fetchedUser.EffectiveAvatarURL())
+		embed.AddField("Account created", fmt.Sprintf("%v (%v days ago)", user.CreatedAt().Format("02.01.2006"), DaysSince(user.CreatedAt())), false)
 
-		if fetchedUser.AccentColor != nil {
-			embed.SetColor(*fetchedUser.AccentColor)
+		embed.SetThumbnail(user.EffectiveAvatarURL())
+
+		if user.AccentColor != nil {
+			embed.SetColor(*user.AccentColor)
 		}
 
-		if fetchedUser.BannerURL() != nil {
+		if user.BannerURL() != nil {
 			formatOpt := SetCDNOptions(discord.FileFormatPNG, discord.QueryValues{"size": 1024})
-			embed.SetImage(*fetchedUser.BannerURL(formatOpt))
+			embed.SetImage(*user.BannerURL(formatOpt))
 		}
-
-		embed.AddField("Account created", fmt.Sprintf("%v (%v days ago)", fetchedUser.CreatedAt().Format("02.01.2006"), DaysSince(fetchedUser.CreatedAt())), false)
 
 		return e.CreateMessage(discord.NewMessageCreateBuilder().SetEmbeds(embed.Build()).Build())
 	}
