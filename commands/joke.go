2a6f111eff1dfea6f8b335871e162312d06109a4
Wokki <iiroseppanen@protonmail.com>
2024-06-10 15:39:23 +0300

    Category choice for joke command

--- commands/joke.go
+++ commands/joke.go
@@ -34,11 +34,57 @@ type JokeRes struct {
 var jokeCommand = discord.SlashCommandCreate{
 	Name:        "joke",
 	Description: "Replies with a random joke",
+	Options: []discord.ApplicationCommandOption{
+		discord.ApplicationCommandOptionString{
+			Name:        "category",
+			Description: "Category of the joke",
+			Required:    false,
+			Choices: []discord.ApplicationCommandOptionChoiceString{
+				{
+					Name:  "Any",
+					Value: "Any",
+				},
+				{
+					Name:  "Programming",
+					Value: "Programming",
+				},
+				{
+					Name:  "Miscellaneous",
+					Value: "Miscellaneous",
+				},
+				{
+					Name:  "Dark",
+					Value: "Dark",
+				},
+				{
+					Name:  "Pun",
+					Value: "Pun",
+				},
+				{
+					Name:  "Spooky",
+					Value: "Spooky",
+				},
+				{
+					Name:  "Christmas",
+					Value: "Christmas",
+				},
+			},
+		},
+	},
 }
 
 func HandleJoke(b *wokkibot.Wokkibot) handler.CommandHandler {
 	return func(e *handler.CommandEvent) error {
-		res, err := b.Client.Rest().HTTPClient().Get("https://v2.jokeapi.dev/joke/Any")
+		data := e.SlashCommandInteractionData()
+
+		var category string
+		if c, ok := data.OptString("category"); ok {
+			category = c
+		} else {
+			category = "Any"
+		}
+
+		res, err := b.Client.Rest().HTTPClient().Get("https://v2.jokeapi.dev/joke/" + category)
 		if err != nil {
 			return e.CreateMessage(discord.NewMessageCreateBuilder().SetContent("Error while fetching joke").Build())
 		}
@@ -60,12 +106,13 @@ func HandleJoke(b *wokkibot.Wokkibot) handler.CommandHandler {
 		embed := discord.NewEmbedBuilder()
 
 		if jokeRes.Joke == "" {
-			embed.SetDescriptionf("%v\n\n||%v||", jokeRes.Setup, jokeRes.Delivery)
+			embed.SetDescriptionf("%v\n\n%v", jokeRes.Setup, jokeRes.Delivery)
 		} else {
 			embed.SetDescription(jokeRes.Joke)
 		}
 
 		embed.SetColor(utils.RGBToInteger(255, 215, 0))
+		embed.SetFooterTextf("ID %v", jokeRes.ID)
 
 		return e.CreateMessage(discord.NewMessageCreateBuilder().SetEmbeds(embed.Build()).Build())
 	}
