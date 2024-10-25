97d985214bc7887c1b8242fdb18398249231c70f
Wokki <iiroseppanen@protonmail.com>
2024-10-25 11:35:34 +0300

    Use h265 conversion

--- commands/download.go
+++ commands/download.go
@@ -105,7 +105,7 @@ func handleDownloadAndConversion(task DownloadTask) {
 	e.UpdateInteractionResponse(discord.NewMessageUpdateBuilder().SetContent("Converting video...").Build())
 	conversion := exec.Command("ffmpeg",
 		"-i", task.filePath,
-		"-c:v", "libx264",
+		"-c:v", "libx265",
 		"-c:a", "aac",
 		"-pix_fmt", "yuv420p",
 		"-f", "mp4",
